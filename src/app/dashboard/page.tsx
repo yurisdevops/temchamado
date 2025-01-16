76b6f8f3467f55473e1bc41243fe7f8953d9e95c
yurisdevops <yurimachado.dev@gmail.com>
2025-01-16 09:26:09 -0300

    First commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/container";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Container>
+        <h1>teste</h1>
+      </Container>
+    </>
+  );
+}

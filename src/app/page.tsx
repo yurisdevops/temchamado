80e5c019d5d19ee7137c8730b1a3e310a119c285
yurisdevops <yurimachado.dev@gmail.com>
2025-01-19 20:19:51 -0300

    Ajuste  titulo da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)]">
       <h2 className="font-medium text-2xl mb-2">Gerencie seus chamados</h2>
-      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl text-center px-3">
         Atendimentos, clientes e serviços
       </h1>
       <Image
         src={hero}
         alt={"Logo da plataforma"}
         width={600}
-        className="max-w-sm md:max-w-xl "
+        className="max-w-sm md:max-w-xl px-10 "
       />
     </main>
   );
